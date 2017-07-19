a27c76ad471d5f69c2894b00e0533d4cf7449ee4
geelee999 <granthatcher52@gmail.com>
2017-07-19 08:54:56 -0400

    prevent submit when blank

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 //this is a js file used for email form
 
 
+
+$('.nwsltr-btn').attr('disabled',true)
+
 $('#email-field').keyup(function(){
   if(this.value !== '' && !isValidEmailAddress(this.value)) {
     $('#email-validation-message').show();
@@ -11,15 +14,21 @@ $('#email-field').keyup(function(){
   }
 });
 
+
+
 $('#email-submit').click(function(e){
-  e.preventDefault();
+  e.preventDefault(); 
   alert('Your email was submited look for great deals in your email');
 });
+
+
+
 
 //Checks if email address is valid.  
 function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
 };
+
 
 
